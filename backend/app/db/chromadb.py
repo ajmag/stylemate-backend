2df886fd82b58d21658ab58cc628cc2b4cc8f312
Anthony <anthonyjm.company@gmail.com>
2025-04-15 13:12:50 -0400

    Adding new database congfigs

---
+++ backend/app/db/chromadb.py
@@ -0,0 +1,21 @@
+import os
+import chromadb
+from app.config import settings
+
+def create_chromadb_client() -> chromadb.Client:
+    """Create and return chromadb client"""
+
+    # make sure directory exitst - will use on local for initial dev
+    os.makedirs(settings.CHROMA_DB_DIR, exist_ok=True)
+
+    # create the presistant client
+    return chromadb.PersistentClient(path=settings.CHROMA_DB_DIR)
+
+def get_or_create_collection(client, collection_name="clothing_items"):
+    """Gets an existing collection or creates a new one if it doesn't exist"""
+    
+    return client.get_or_create_collection(name=collection_name)
+
+
+chroma_client = create_chromadb_client()
+clothing_collection = get_or_create_collection(chroma_client)
